31b4551c26df3b87eefcd6953af6bab3f4c4164e
Dongho Gwak <wing.flower.k@gmail.com>
2024-06-21 17:08:22 +0900

    feat: Settings 분리, Postgresql 적용 및 연동

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # django-db-nginx-pgadmin-adminer
+
+- PYTHON VERSION [3.10.14]
+
+# RUN
+- docker-compose build --no-cache && docker-compose up -d && docker-compose logs -f
--- app/app/settings/local.py
+++ app/app/settings/local.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,24 +21,84 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)cti5f!g+vzk3k)yf9%79z5d3o25$845du-0y(tnr093&0b$#'
+# Use secret file
+# secret_file = os.path.join(BASE_DIR, 'secrets.json')
+# 
+# with open(secret_file) as f:
+#     secrets = json.loads(f.read())
+# 
+# def get_secret(setting):
+#     """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+#     try:
+#         return secrets[setting]
+#     except KeyError:
+#         error_msg = 'Set the {} environment variable'.format(setting)
+#         raise ImproperlyConfigured(error_msg)
+# 
+# 
+# SECRET_KEY = get_secret('SECRET_KEY')
+# Use docker env
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-b)cti5f!g+vzk3k)yf9%79z5d3o25$845du-0y(tnr093&0b$#')
+# SECRET_KEY = 'django-insecure-b)cti5f!g+vzk3k)yf9%79z5d3o25$845du-0y(tnr093&0b$#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+]
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'authorization',
+    'content-type',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'authorization',
+    'origin',
+)
+# CORS_ALLOWED_ORIGIN_REGEXES = [
+#     r'^https://\w+\.example\.com$',
+# ]
+# CORS_URLS_REGEX = r'^/api/.*$'
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+CSRF_TRUSTED_ORIGINS = [
+    'http://0.0.0.0'
+]
 
 # Application definition
 
-INSTALLED_APPS = [
+SHARED_APPS = (
+#    'django_tenants',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+#    'django_filters',
+    'rest_framework',
+    'corsheaders',
+    'drf_yasg',
+#    'django_extensions',
+    'app'
+)
+
+TENANT_APPS = (
+)
+
+INSTALLED_APPS = list(SHARED_APPS)\
+                  + [app for app in TENANT_APPS if app not in SHARED_APPS]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,9 +108,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
+#PUBLIC_SCHEMA_URLCONF = 'app.urls_public'
 
 TEMPLATES = [
     {
@@ -73,10 +136,20 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DJANGO_DB_ENGINE','django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DJANGO_DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get('DJANGO_DB_USER', 'user'),
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', 'password'),
+        'HOST': os.environ.get('DJANGO_DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DJANGO_DB_PORT', '5432'),
     }
 }
 
@@ -103,9 +176,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -114,7 +188,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, '../../static/')
 STATIC_URL = 'static/'
 
 # Default primary key field type
